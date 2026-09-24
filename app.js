d3ff2d09183574f2dd0505574a9bf1c0fae4494e
atuljain500 <50399605+atuljain500@users.noreply.github.com>
2026-09-24 20:39:05 +0530

    updated text sirsa

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ console.log("hey")
 console.log("hello world");
 console.log("what is your name");
 console.log("bighar");
-console.log("sirsa");
+console.log("bye sirsa");
 console.log("hello mr atul");
